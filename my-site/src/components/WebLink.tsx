04c91180911b9a8b33cc74b8f81ce488291a396d
Matt Wiley <mattwiley+gitlab@fastmail.com>
2023-10-02 16:19:36 -0400

    Corrected link hover behavior.

--- my-site/src/components/WebLink.tsx
+++ my-site/src/components/WebLink.tsx
@@ -10,14 +10,14 @@ const WebLink = (props: IWebLinkProps) => {
   const defaultIcon = 'fa-globe'
 
   return (
-    <div className="inline-block w-full flex flex-row justify-start align-middle text-black hover:text-blue-400">
+    <div className="inline-block w-full flex flex-row justify-start align-middle">
       <div className="w-5 h-5 mr-2 text-center">
         { (!props.image) ? 
           <i className={`${props.icon || defaultIcon}`} /> : 
           <img src={`${ props.image }`} className="w-5 h-5 mr-2" /> 
         }
       </div>
-      <a href={props.link}>{props.text}</a>
+      <a className="text-black hover:text-blue-400" href={props.link}>{props.text}</a>
     </div>
   );
 };
