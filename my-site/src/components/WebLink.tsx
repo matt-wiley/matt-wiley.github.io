7bc720332471121843c0568fc4b2d70518b3b181
Matt Wiley <mattwiley+gitlab@fastmail.com>
2023-10-02 16:42:57 -0400

    Handling lint warnings.

--- my-site/src/components/WebLink.tsx
+++ my-site/src/components/WebLink.tsx
@@ -10,11 +10,11 @@ const WebLink = (props: IWebLinkProps) => {
   const defaultIcon = 'fas fa-globe'
 
   return (
-    <div className="inline-block w-full flex flex-row justify-start align-middle">
+    <div className="w-full flex flex-row justify-start align-middle">
       <div className="w-5 h-5 mr-2 text-center">
         { (!props.image) ? 
           <i className={`${props.icon || defaultIcon}`} /> : 
-          <img src={`${ props.image }`} className="w-5 h-5 mr-2" /> 
+          <img src={`${ props.image }`} alt={props.text} className="w-5 h-5 mr-2" /> 
         }
       </div>
       <a className="text-black hover:text-blue-400" href={props.link}>{props.text}</a>
